ae9807873f4567b4eebe1759fa621f082ba49bdd
Xenonstory <simon.j.hajjar@gmail.com>
2016-01-03 05:04:57 -0500

    finished karp-rabin algorithm

--- Karp-Rabin/karp_rabin.cc
+++ Karp-Rabin/karp_rabin.cc
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <string>
 
 int karp_rabin(std::string * s, std::string * t) {
@@ -18,8 +17,9 @@ int karp_rabin(std::string * s, std::string * t) {
     const int STR_BASE = 128;
 
     // We also need a prime number to keep our division method a constant time
-    // amortized positive result (i.e. less collisions)
-    const int PRIME = 7919;
+    // amortized positive result (i.e. less collisions). Note that there is
+    // a maximum number of bits for an int too which will be module a 
+    // Mersenne Prime, so no manual modulus is required.
     
     // We first calculate the rolling hash of s and t. Note that according to
     // the C++ standard, s->length() is a constant complexity function, so
@@ -27,10 +27,12 @@ int karp_rabin(std::string * s, std::string * t) {
     // Rabin fingerprint rolling hash
     int hash_s = 0, hash_t = 0, multiplier = 1;
     for(int i = s->length() - 1; i >= 0; i--) {
-        hash_s += ((int)(*s)[i] * multiplier);
-        hash_t += ((int)(*t)[i] * multiplier);
+        hash_s += (*s)[i] * multiplier;
+        hash_t += (*t)[i] * multiplier;
         multiplier *= STR_BASE;
     }
+
+    // Re-adjust the multiplier for ease of us when adding new digits later
     multiplier /= STR_BASE;
 
     // First check if the substring is found at the first index
@@ -71,12 +73,26 @@ int karp_rabin(std::string * s, std::string * t) {
     return -1;
 }
 
+#include <iostream>
+#include <time.h>
+using namespace std;
+
 int main() {
     /*
-        An example of use for the karp-rabin algorithm
+        An example of use for the karp-rabin algorithm. We load a file which
+        contains randomly generated Latin
     */
-    std::string pattern = "aaaaaaaaa";
-    std::string document = "bbbbaaaaaaaaaaa Franciscoaa";
-    std::cout << karp_rabin(&pattern, &document) << std::endl;
+    std::string pattern = "ab";
+    std::string document = "Karp-Rabin Algorithm";
+
+    // Time how long it takes to find the substring
+    clock_t start, end;
+    start = clock();
+    int result = karp_rabin(&pattern, &document);
+    end = clock();
+
+    // Print out the result
+    cout << "Found substring at index " << result << " in ";
+    cout << ((float)end - (float)start) << " cycles" << endl;
     return 0;
 }
