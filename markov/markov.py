0ade721ad1939e7129b5a04b404fffab7e437ca8
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-03-13 19:08:15 -0700

    made markov second order

--- markov/markov.py
+++ markov/markov.py
@@ -1,110 +1,92 @@
-from json import dump, load
-from random import choice, randint
-from collections import defaultdict
+from random import randint
 
 class Markov:
     """
-    An implementation of a markov graph. Current implementation is first order,
-    using an adjacency list to represent the directed acyclic graph.
-    Intended for ASCII text (i.e. won't filter characters like \xe2)
+    An implementation of a second order Markov chain. Represents the directed
+    acyclic graph as an adjacency list.
     """
 
-    punctuation = [".", ",", "'", "\"", "(", ")", ";", " "]
-    minimum_sentence_length = 5
-    save_file = "markov_save.json"
+    EOF = [".", "\n"]
+    SPECIAL_CHARS = [",", "(", ")"]
 
     def __init__(self):
-        self.graph = defaultdict(lambda : defaultdict(int))
-
-        # We add a \n character as part of the graph to indicate the end
-        self.graph["\n"] = {}
-        
-        # We now load the previously learned data
-        try:
-            with open(Markov.save_file) as data_file:
-                loaded_data = load(data_file)
-                for word in loaded_data:
-                    for next_word in loaded_data[word]:
-                        self.graph[word][next_word] = loaded_data[word][next_word]
-        except IOError:
-            # No previous store was found. No worries, we just keep it empty
-            pass        
+        """
+        Creates a new Markov chain.
+        """
+        # We represent the graph has a dictionary of dictionaries. The entry
+        # "." is used as both the end and the beginning of a sentence.
+        self.graph = {}
 
     @staticmethod
-    def clean_word(word):
+    def clean(word):
         """
-        Removes punctuation from the given word, and removes capital letters
+        Returns a cleaned version of the given word.
         """
-        for element in Markov.punctuation:
-            word = word.replace(element, "")
-        return word.lower()
+        # Remove any "special" characters that we don't want
+        for char in Markov.SPECIAL_CHARS:
+            word = word.replace(char, "")
+        return word.strip()
 
-    def convert_graph(self, ddict):
+    def addWord(self, recent_word, last_word, word):
         """
-        Converts the current graph to a normal dictionary for JSON's sake 
+        Adds the new word into the known dictionary.
         """
-        if isinstance(ddict, defaultdict):
-            ddict = {key: self.convert_graph(value) for key, value in ddict.iteritems()}
-        return ddict
-
-    def save(self):
-        """
-        Saves the current data into a text file
-        """
-        converted = self.convert_graph(self.graph)
-        dump(converted, open(self.save_file, "w"))
+        if not (recent_word, last_word) in self.graph:
+            self.graph[(recent_word, last_word)] = {}
+        if not word in self.graph[(recent_word, last_word)]:
+            self.graph[(recent_word, last_word)][word] = 0
+        self.graph[(recent_word, last_word)][word] += 1
 
     def learn(self, filename):
+        """ 
+        Learns the given data for later generation.
         """
-        Given a file, learns the text inside of it and adds it to the graph.
-        """
-        with open(filename) as text_file:
-            for line in text_file:
-                # Generate the list of words, without the newline character.
-                # We remove all commas and periods.
-                words = [Markov.clean_word(word) for word in line[:-1].split(" ")]
-                
-                # Add a unit of weight to an edge if we find a connection
-                for number, word in enumerate(words):
-                    if number == len(words) - 1:
-                        self.graph[word]["\n"] += 1
-                    else:
-                        self.graph[word][words[number + 1]] += 1
+        last_word, recent_word = Markov.EOF[0], Markov.EOF[0]
+        with open(filename) as data:
+            for entry in data:
+                # Take each entry line by line and enter it into our graph
+                words = [Markov.clean(word) for word in entry.split(" ")]
+                for word in words:
+                    # Take note if its an end of line
+                    EOF_found = False
+                    if word[-1] in Markov.EOF:
+                        word = word[:-1]
+                        EOF_found = True
 
-    def generate_next_word(self, word):
+                    # Learn the new word
+                    self.addWord(recent_word, last_word, word)
+                    if EOF_found:
+                        self.addWord(last_word, word, Markov.EOF[0])
+                        recent_word, last_word = Markov.EOF[0], Markov.EOF[0]
+                    else:
+                        recent_word, last_word = last_word, word
+
+    def generate_word(self, recent_word, last_word):
         """
-        Returns a randomly selected next-word based on the given word.
+        Generates a word based on the two given preceding words.
         """
-        # Keep generating words until an end-of-sentence is come across
-        total_weight = sum(self.graph[word][key] for key in self.graph[word].keys())
-        next_word = randint(0, total_weight)
-        for key in self.graph.keys():
-            if next_word <= self.graph[word][key]:
-                return "" if key == "\n" else key
-            else:
-               next_word -= self.graph[word][key]
+        current_graph = self.graph[(recent_word, last_word)]
+        total_weight = sum(current_graph[next_word] for next_word in current_graph.keys())
+        selected = randint(0, total_weight)
+        for next_word in current_graph.keys():
+            if selected <= current_graph[next_word]:
+                return next_word
+            selected -= current_graph[next_word]
 
     def generate_sentence(self):
         """
-        Generates a sentence based on what self has learned so far.
+        Generates a sentence based on what has been learned thus far.
         """
-        # Currently chooses a random word as a starting word
-        current = choice(self.graph.keys())
-        sentence = current.capitalize() + " "
-        sentence_length = 1
-        while current:
-            try:
-                current = self.generate_next_word(current)
-            except KeyError:
-                raise Exception("No data has been learned yet.")
-            while sentence_length < Markov.minimum_sentence_length and not current:
-                current = choice(self.graph.keys())
-            sentence += current + " "
-            sentence_length += 1
-        return sentence.strip() + "."
+        recent_word, last_word = Markov.EOF[0], Markov.EOF[0]
+        next_word = self.generate_word(recent_word, last_word)
+        sentence = ""
+        while next_word != ".":
+            next_word = self.generate_word(recent_word, last_word)
+            sentence += next_word + " "
+            recent_word, last_word = last_word, next_word
+        return sentence[:-2].strip() + Markov.EOF[0]
 
 if __name__ == '__main__':
-    # An example of the Markov chain in action with Apple's terms and conditions
     markov = Markov()
-    markov.learn("apple.txt")
+    markov.learn("testfile.txt")
     print markov.generate_sentence()
