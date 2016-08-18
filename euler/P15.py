df1014a7bece215f15d831921445e4f0dc5c827d
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-08-18 16:16:31 -0400

    finished P15

---
+++ euler/P15.py
@@ -0,0 +1,17 @@
+"""
+Lattice Paths
+Problem 15
+"""
+
+from math import factorial
+
+DIM = 20
+
+if __name__ == '__main__':
+    # To go from the top left corner to the bottom right corner of a 20 x 20
+    # grid, only moving left and down, you must move left exactly 20 times,
+    # and down exactly 20 times. Thus there are 40 moves, 20 of which you must
+    # choose to be downwards, and so the solution is 40 choose 20
+    numerator = reduce(lambda x, y: x * y, range(DIM + 1, 2 * DIM + 1))
+    denominator = reduce(lambda x, y: x * y, range(1, DIM + 1))
+    print numerator / denominator
