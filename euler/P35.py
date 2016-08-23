0ff4bc8a55626d08e30cf806949f0f500cf91522
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-08-22 20:03:07 -0400

    sped up 35

--- euler/P35.py
+++ euler/P35.py
@@ -4,14 +4,17 @@ Problem 35
 """
 
 UPPER_BOUND = 1000000
+EVEN_DIGITS = ['0', '2', '4', '6', '8']
 
 def sieve():
     # Runs the sieve of eratosthenes to find prime numbers up to UPPER_BOUND
-    erato, primes = [True] * UPPER_BOUND, {}
+    erato, primes = [True] * UPPER_BOUND, {'2' : True}
     for i in xrange(2, UPPER_BOUND):
         if erato[i]:
-            # We found a prime, so switch off all multiples
-            primes[str(i)] = True
+            # We found a prime, so switch off all multiples. The prime is only
+            # valid if it does not contain even digits.
+            if not any(j in str(i) for j in EVEN_DIGITS):
+                primes[str(i)] = True
             for j in xrange(i, UPPER_BOUND, i):
                 erato[j] = False
     return primes
