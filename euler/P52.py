f3148a16b5faecdc700340d3e45a3932efc094e7
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-08-26 16:22:11 -0400

    cleaned up 52

--- euler/P52.py
+++ euler/P52.py
@@ -3,15 +3,12 @@ Permuted Multiples
 Problem 52
 """
 
-MULT_BOUND = 7
+BOUND = 7
 
 if __name__ == '__main__':
     # This program uses a brute-force solution, with the one optimization that
     # it stops early if one of the multiples don't match thanks to all()
-    found, x = False, 1
-    while not found:
-        if all(set(str(x * i)) == set(str(x)) for i in range(2, MULT_BOUND)):
-            found = True
-            print x
-        else:
-            x += 1
+    x = 1
+    while not all(set(str(x * i)) == set(str(x)) for i in range(2, BOUND)):
+        x += 1
+    print x
