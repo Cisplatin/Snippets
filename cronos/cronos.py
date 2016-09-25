d21bf15729d84055d5415121e1b8b361631259a7
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-09-25 16:33:35 -0400

    __map() for Cronos

--- cronos/cronos.py
+++ cronos/cronos.py
@@ -1,3 +1,6 @@
+from struct import pack
+from pickle import dump
+
 class Cronos:
     """
     Cronos is a compression function that is designed specifically for use on
@@ -7,7 +10,22 @@ class Cronos:
     https://github.com/first20hours/google-10000-english
     """
 
-    # The words, in order of frequency, appear in WORDS_TXT delimited by \n
-    WORDS_TXT = "WORDS.txt"
-    MAPPED_WORDS_TXT = "MAPPED_WORDS.txt"
+    # The words, in order of frequency, appear in WORDS_TXT delimited by \n.
+    # Words with their mapped n-byte output are placed into MAPPED_WORDS_TXT.
+    WORDS_TXT = 'WORDS.txt'
+    MAPPED_WORDS_P = 'MAPPED_WORDS.p'
 
+    # This indicates that we store keys as little-endian unsigned ints
+    PACK_FORMATTING = ">I"
+
+    # Re-maps all words in WORDS_TXT to MAPPED_WORDS_TXT
+    @staticmethod
+    def __map():
+        translation, current_byte = {}, 0
+        with open(Cronos.WORDS_TXT, 'r') as words:
+            for word in words.read().split('\n')[:-1]:
+                translation[word] = pack(">I", current_byte)
+                current_byte += 1
+        dump(translation, open(Cronos.MAPPED_WORDS_P, "wb"))
+
+Cronos._Cronos__map()
