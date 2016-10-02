5b09009c6d258257600b64446f8d8a01442c096d
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-10-02 15:04:49 -0400

    cleaned up P14

--- euler/python/P14.py
+++ euler/python/P14.py
@@ -3,27 +3,18 @@ Longest Collatz Sequence
 Problem 14
 """
 
-MAX = 1000000
+MAX_START = 1000000
 
 if __name__ == '__main__':
     # Keep all known chain lengths in a hash
-    chain_lengths = {1 : 1}
+    chain_lengths = {0 : 0, 1 : 1}
 
     # A recursive function to find and update collatz chains
     def collatz(i):
         if not i in chain_lengths:
-            if i % 2 == 0:
-                result = collatz(i / 2)
-            else:
-                result = collatz(3 * i + 1)
-            chain_lengths[i] = result + 1
+            next_item = i / 2 if i % 2 == 0 else 3 * i + 1
+            chain_lengths[i] = collatz(next_item) + 1
         return chain_lengths[i]
 
-    # Find the values for all numbers up to MAX
-    max_chain_length, max_chain_number = 0, 0
-    for num in range(1, MAX):
-        length = collatz(num)
-        if length > max_chain_length:
-            max_chain_length, max_chain_number = length, num
-    print max_chain_number
-
+    # Find the values for all numbers up to MAX_START
+    print max((collatz(i), i) for i in xrange(MAX_START))[1]
