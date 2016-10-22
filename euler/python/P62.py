8f78cb67c3b61ecd03ff3a4cc8306afc9fbf0c13
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-10-22 18:45:56 -0400

    changes to p62

--- euler/python/P62.py
+++ euler/python/P62.py
@@ -3,7 +3,9 @@ Cubic Permutations
 Problem 62
 """
 
+POWER = 3
 PERMUTATIONS = 5
+BASE = 10
 
 if __name__ == '__main__':
     # We go through each cube until we find one with three permutations
@@ -11,30 +13,31 @@ if __name__ == '__main__':
 
     # We keep track of the number of permutations for current, as well as the current maximum
     # so that we can update the number of digits
-    found_permutations, current_max = 0, 10
-    while len(permutations[found_permutations]) != PERMUTATIONS:
+    current_key, current_max = 0, BASE
+    while len(permutations[current_key]) != PERMUTATIONS:
         # Increment our current and find the cube
         current += 1
-        cube = current ** 3
+        cube = current ** POWER
         sorted_cube = str(sorted(str(cube)))
 
         # If we get more digits we can reset the permutations
         if cube > current_max:
             permutations = {}
-            current_max *= 10
+            current_max *= BASE
 
         # Check if we already found a permutation
-        found_permutations = -1
+        current_key = -1
         for other_permutation in permutations:
             if sorted_cube == other_permutation:
-                permutations[other_permutation].append(current)
-                found_permutations = other_permutation
+                current_key = other_permutation
+                permutations[current_key].append(current)
                 break
 
         # If we didn't find a permutation, add a new one
-        if found_permutations == -1:
-            found_permutations = str(sorted(str(cube)))
-            permutations[found_permutations] = [current]
+        if current_key == -1:
+            current_key = str(sorted(str(cube)))
+            permutations[current_key] = [current]
 
-    # Print the cube of the minimum number we found
-    print min([found_permutations] + permutations[found_permutations]) ** 3
+    # Print the cube of the minimum number we found. Since we add them in order, the first
+    # element will be the minimum.
+    print permutations[current_key][0] ** POWER
