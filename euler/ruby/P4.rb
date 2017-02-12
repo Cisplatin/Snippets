1f5a87c917450d6b3e5fbeb8ea52f9b4e41b1154
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-02-12 06:57:54 -0500

    fixed P4 in ruby

--- euler/ruby/P4.rb
+++ euler/ruby/P4.rb
@@ -1,9 +1,18 @@
-"""
+"
 Problem 4
 Largest Palindrome Product
-"""
+"
 
-# We use pure functional programming. Pretty read-able, but the gist is that
-# it takes every combonation of two-digit numbers, multiplies it, then finds
-# the maximal result.
-puts (100..999).to_a.combination(2).map{|n| n.inject(:*)}.select{|n| n.to_s.reverse.to_i == n}.max
+NUMBER_BASE = 10
+NUM_OF_DIGITS = 3
+PRODUCT_DEGREE = 2
+
+# Find the range of numbers we want to work in (the maximum is exclusive)
+minimum = NUMBER_BASE ** (NUM_OF_DIGITS - 1)
+maximum = minimum * NUMBER_BASE
+
+puts (minimum...maximum)
+  .to_a.combination(PRODUCT_DEGREE)        # Find combinations of two numbers
+  .map { |n| n.inject(:*) }                # Multiply the numbers together
+  .select { |n| n.to_s.reverse.to_i == n } # Filter non-palindrome multiples
+  .max                                     # Find the maximum
