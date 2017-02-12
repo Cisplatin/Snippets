9ec2216688c50e4259cfa051812750c8496afee0
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-02-12 08:01:11 -0500

    fixed P6 in ruby

--- euler/ruby/P6.rb
+++ euler/ruby/P6.rb
@@ -1,9 +1,14 @@
-"""
-  Sum Square Difference
-  Problem 6
-"""
+"
+Sum Square Difference
+Problem 6
+"
 
 MINIMUM = 1
 MAXIMUM = 100
 
-puts (MINIMUM..MAXIMUM).inject(:+) ** 2 - (MINIMUM..MAXIMUM).map{ |i| i ** 2 }.inject(:+)
+# Calculate the square of the sum and the sum of the squares seperately, then
+# print out the difference. We know that the square of the sum will be larger
+# so we subtract from that number.
+square_of_sum = (MINIMUM..MAXIMUM).inject(:+) ** 2
+sum_of_square = (MINIMUM..MAXIMUM).map { |i| i ** 2 }.inject(:+)
+puts square_of_sum - sum_of_square
