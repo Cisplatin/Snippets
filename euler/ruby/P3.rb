aa74fb6ebadada7e033fc407d9e4a6183ee1cd8b
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-02-12 06:47:35 -0500

    fixed P3 in ruby

--- euler/ruby/P3.rb
+++ euler/ruby/P3.rb
@@ -1,17 +1,19 @@
-"""
+"
 Problem 3
 Largest Prime Factor
-"""
+"
 
-LARGEST_PRIME_OF = 600851475143
 LOWEST_PRIME = 2
+LARGEST_PRIME_OF = 600_851_475_143
 
 # Keep finding primes that divide LARGEST_PRIME_OF until there are no more
-prime, decompose, current = 0, LARGEST_PRIME_OF, LOWEST_PRIME
+prime = 0
+decompose = LARGEST_PRIME_OF
+current = LOWEST_PRIME
 while decompose > 1
-    # If it divides, we replace our current largest prime with this one
-    prime = current if decompose % (current += 1) == 0
-    decompose /= current while decompose % current == 0
+  # If it divides, we replace our current largest prime with this one
+  prime = current if (decompose % (current += 1)).zero?
+  decompose /= current while (decompose % current).zero?
 end
 
 # Output the final answer
