e1b0a2877403f58e36521c9fc54c53eeb616f2a3
Simon Hajjar <simon.j.hajjar@gmail.com>
2017-02-12 06:58:54 -0500

    fixed P5 in ruby

--- euler/ruby/P5.rb
+++ euler/ruby/P5.rb
@@ -1,9 +1,11 @@
-"""
-  Smallest Multiple
-  Problem 5
-"""
+"
+Smallest Multiple
+Problem 5
+"
 
 MINIMUM = 1
 MAXIMUM = 20
 
+# The smallest positive number that divides all the numbers requested is
+# equivalent to the lowest common multiplier
 puts (MINIMUM...MAXIMUM).inject(&:lcm)
